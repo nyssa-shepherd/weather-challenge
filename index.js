d23b99437579b716ec98426793ee0fa9466a542a
Nyssa Keller <nyssa1@gmail.com>
2018-08-10 09:08:21 -0600

    Create function to format data from the api

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const search = async() => {
+$('#search-btn').on('click', search);
+
+async function search() {
   const zip = $('#search-input').val();
   const initialFetch = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=e8f0baa7772713571ca243de47d6139d&zip=${zip}`);
-  const data = await initialFetch.json();
-  console.log(data);
+  const weatherData = await initialFetch.json();
+  cleanData(weatherData)
 }
 
-$('#search-btn').on('click', search);
+function cleanData(weatherData) {
+  const forecastObj = weatherData.list.reduce((weatherObj, forecast) => {
+    const date = forecast.dt_txt.split(' ')[0];
+    !weatherObj[date] ? weatherObj[date] = [forecast] : weatherObj[date] = [...weatherObj[date], forecast];
+    return weatherObj;
+  }, {});
+  console.log(forecastObj);
+}
